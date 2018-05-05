4ceb273d500c2f19076a13571218465f2f7a6bfc
felix-b <felixberman76@gmail.com>
2018-05-05 23:01:59 +0300

    Try Buildalyzer for loading workspace in RoslynCodeModelReader

--- Source/MetaPrograms.Adapters.Roslyn/RoslynCodeModelReader.cs
+++ Source/MetaPrograms.Adapters.Roslyn/RoslynCodeModelReader.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 using Buildalyzer;
 using Buildalyzer.Workspaces;
 using MetaPrograms.CodeModel.Imperative;
 using MetaPrograms.CodeModel.Imperative.Members;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace MetaPrograms.Adapters.Roslyn
 {
@@ -16,7 +20,40 @@ namespace MetaPrograms.Adapters.Roslyn
         public void AddProject(string projectFilePath)
         {
             ProjectAnalyzer analyzer = _buildalyzer.GetProject(projectFilePath);
-            AdhocWorkspace workspace = analyzer.GetWorkspace();         
+            AdhocWorkspace workspace = analyzer.GetWorkspace();
+
+            var solution = workspace.CurrentSolution;
+            var projects = solution.Projects.ToArray();
+            var firstProject = projects[0];
+            var compilation = firstProject.GetCompilationAsync(CancellationToken.None).Result;
+            var syntaxTrees = compilation.SyntaxTrees.ToArray();
+
+            foreach (var tree in syntaxTrees)
+            {
+                var semanticModel = compilation.GetSemanticModel(tree);
+
+                var topLevelClasses = tree.GetCompilationUnitRoot()
+                    .DescendantNodes(descendIntoChildren: node => {
+                        var result = (node is CompilationUnitSyntax || node is NamespaceDeclarationSyntax);
+                        return result;
+                    })
+                    .OfType<ClassDeclarationSyntax>()
+                    .ToList();
+
+                foreach (var singleClass in topLevelClasses)
+                {
+                    _types.Add(ParseClass(semanticModel, singleClass));
+                }
+            }
+        }
+
+        private TypeMember ParseClass(SemanticModel semanticModel, ClassDeclarationSyntax syntax)
+        {
+            var typeSymbol = semanticModel.GetDeclaredSymbol(syntax);
+            var builder = new TypeMemberBuilder();
+            builder.Namespace = typeSymbol.ContainingNamespace?.Name;
+            builder.Name = typeSymbol.Name;
+            return new TypeMember(builder);
         }
 
         public void Read()
