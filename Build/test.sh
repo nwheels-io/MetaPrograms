33e90befda956ed9fb8b3f0703e699731b204d66
Felix Berman <felixberman76@gmail.com>
2018-10-18 13:05:21 +0300

    Add uploading of coverage reports to codecov

---
+++ Build/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+script_dir=`dirname "$0"` 
+cd $script_dir/../Source 
+
+for project_name in $(find . -type d -name '*Tests' | cut -c3-)
+do
+    dll_path=./$project_name/bin/Release/netcoreapp2.0/$project_name.dll
+    echo "--- $project_name ---"
+    dotnet test $project_name /p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:CoverletOutput=./$project_name.opencover.xml
+done
+
+find . -type f -name '*.opencover.xml' | xargs -n1 ./codecov.sh
