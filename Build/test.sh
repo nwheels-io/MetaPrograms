73ef45272e65f5d9e29f01548cccb3875df91782
Felix Berman <felixberman76@gmail.com>
2018-10-18 14:35:15 +0300

    Fix appveyor.yml (9)

--- Build/test.sh
+++ Build/test.sh
@@ -10,4 +10,4 @@ do
     dotnet test $project_name /p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:CoverletOutput=./$project_name.opencover.xml
 done
 
-find . -type f -name '*.opencover.xml' | xargs -n1 $script_dir/codecov.sh -f 
+find . -type f -name '*.opencover.xml' | xargs -n1 $script_dir/codecov.sh -t $CODECOV_TOKEN -f 
