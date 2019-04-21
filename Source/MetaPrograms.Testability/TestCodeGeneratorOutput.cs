689573dd19856c15336cab04e07f9d9308d87730
Felix Berman <felixb@wix.com>
2019-04-21 07:25:50 +0300

    Reorganize demos + extract MetaPrograms.Testability

--- Source/MetaPrograms.Testability/TestCodeGeneratorOutput.cs
+++ Source/MetaPrograms.Testability/TestCodeGeneratorOutput.cs
@@ -1,15 +1,16 @@
-﻿using System.Collections.Immutable;
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.IO;
+using System.Linq;
+using MetaPrograms.Extensions;
+using Shouldly;
 
 namespace MetaPrograms.Testability
 {
     public class TestCodeGeneratorOutput : ICodeGeneratorOutput
     {
-        public TestCodeGeneratorOutput()
-        {
-            SourceFiles = ImmutableDictionary<string, Stream>.Empty;
-        }
-
+        private readonly List<FileEntry> _sourceFiles = new List<FileEntry>();
+        
         public void AddSourceFile(FilePath path, string contents)
         {
             var stream = new MemoryStream();
@@ -18,11 +19,70 @@ namespace MetaPrograms.Testability
             writer.Write(contents);
             writer.Flush();
 
-            SourceFiles = SourceFiles.Add(path.NormalizedFullPath, stream);
+            stream.Position = 0;
+            _sourceFiles.Add(new FileEntry(path, stream));
+        }
+
+        public IDictionary<string, Stream> IndexSourceFilesByNormalPath()
+        {
+            return _sourceFiles.ToDictionary(f => f.Path.NormalizedFullPath, f => f.Contents);
         }
 
         public CodeTextOptions TextOptions => CodeTextOptions.Default;
 
-        public ImmutableDictionary<string, Stream> SourceFiles { get; private set; }
+        public IReadOnlyList<FileEntry> SourceFiles => _sourceFiles; 
+        
+        public void WriteToDisk(string baseFolderPath)
+        {
+            foreach (var entry in _sourceFiles)
+            {
+                var filePath = Path.Combine(baseFolderPath, entry.Path.FullPath);
+                var folderPath = Path.GetDirectoryName(filePath);
+
+                if (!Directory.Exists(folderPath))
+                {
+                    Directory.CreateDirectory(folderPath);
+                }
+
+                using (var file = File.Create(filePath))
+                {
+                    entry.Contents.Position = 0;
+                    entry.Contents.CopyTo(file);
+                    file.Flush();
+                }
+            }
+        }
+        
+        private void ShouldMatchFolder(string expectedOutputsFolder)
+        {
+            var expectedFilePaths =
+                Directory.GetFiles(expectedOutputsFolder, "*.*", SearchOption.AllDirectories)
+                    .Select(path => path.TrimSuffix(expectedOutputsFolder))
+                    .Select(path => path.TrimStart(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar))
+                    .ToArray();
+
+            var actualFilePaths =_sourceFiles.Select(f => f.Path.FullPath);
+            actualFilePaths.ShouldBe(expectedFilePaths, ignoreOrder: true);
+            
+            foreach (var entry in _sourceFiles)
+            {
+                var expectedFilePath = Path.Combine(expectedOutputsFolder, entry.Path.FullPath);
+                var actualStream = entry.Contents;
+                actualStream.Position = 0;
+                actualStream.ShouldMatchTextFile(expectedFilePath);
+            }
+        }
+
+        public class FileEntry
+        {
+            public FileEntry(FilePath path, Stream contents)
+            {
+                Path = path;
+                Contents = contents;
+            }
+
+            public readonly FilePath Path;
+            public readonly Stream Contents;
+        }
     }
 }
